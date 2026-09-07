a8002a32bd4035a48bd3e25feb1b283a9e76e8ae
Sohan-2807 <thamminenisohan1977@gmail.com>
2026-09-07 15:13:01 +0530

    sum of Diagonal Elements

--- CPP/ArrayFunctions.cpp
+++ CPP/ArrayFunctions.cpp
@@ -73,7 +73,6 @@ int main() {
 
     ob.getData();
     ob.showData();
-
     ob.findMax();
 
     int e;
---
+++ CPP/SumofDiagonal.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Size of Square matrix: ";
+    cin>>n;
+    int arr[n][n];
+    int sum = 0;
+    cout<<"\nEnter array of "<<n*n<< " Elements: ";
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0 ;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"\nEntered Array of "<<n<<" by "<<n<<" Array is: "<<endl;
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0 ;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    
+    for(int i = 0;i<n;i++){
+        for(int j = 0 ;j<n;j++){
+            if(i==j){
+                sum += arr[i][j];
+            }
+            else continue;
+        }
+    }
+
+    cout<<"\nSum of Diagonal elements is: "<<sum;
+
+
+}
