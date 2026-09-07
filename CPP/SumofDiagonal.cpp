188e30deb26b5d939e21de6d24cff22623c4c5c0
Sohan-2807 <thamminenisohan1977@gmail.com>
2026-09-07 16:04:48 +0530

    Insertion Sort

--- CPP/SumofDiagonal.cpp
+++ CPP/SumofDiagonal.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main(){
     int n;
-    cout<<"Enter Size of Square matrix: ";
+    cout<<"Enter Size of Square matrix row: ";
     cin>>n;
     int arr[n][n];
     int sum = 0;
-    // cout<<"\nEnter array of "<<n*n<< " Elements: ";
+    cout<<"\nEnter array of "<<n*n<< " Elements: ";
 
     for(int i = 0;i<n;i++){
         for(int j = 0 ;j<n;j++){
             cin>>arr[i][j];
         }
     }
-    // cout<<"\nEntered Array of "<<n<<" by "<<n<<" Array is: "<<endl;
+    cout<<"\nEntered Array of "<<n<<" by "<<n<<" Array is: "<<endl;
 
     for(int i = 0;i<n;i++){
         for(int j = 0 ;j<n;j++){
